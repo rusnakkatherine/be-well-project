512b05e8cfe9198f7f75466f0a2797916bfe1303
pravallika <pravallikaammineni@github.com>
2019-05-21 20:39:19 -0700

    time and reminders

--- js/script.js
+++ js/script.js
@@ -1,17 +1,2 @@
-// function myFunction() {
-//     var reminderTime = "";
-//     var i;
-//     for (i = 0; i <= 24; i++) {
-//         reminderTime += "Time is " + i + "<br>";
-//     }
-//     document.getElementById("demo").innerHTML = reminderTime;
-//   }
 
-// function myFunction() {
-//     var text = prompt("Please enter your reminder", "Harry Potter");
-//     if (person != null) {
-//       document.getElementById("demo").innerHTML =
-//       "Hello " + person + "! How are you today?";
-//     }
-//   }
 
