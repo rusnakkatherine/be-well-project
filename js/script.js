f784761f6c5f401eab940c060894b072b0719bbc
pravallika <pravallikaammineni@github.com>
2019-06-03 20:44:36 -0700

    added paragraph clear and submit buttons

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ function repeat(n,code){
     }
 }
 
-function vn(){
+function vm(){
 
-    var kd='<div class=" col-md-6 "> \
+    var morning='<div class=" col-md-6 "> \
     <label for="Reminder-choice">Reminders:</label> \
         <input list="Reminders-list" id="Reminder-choice" name="Reminder-choice" /> \
                     <!-- <select  > --> \
@@ -48,10 +48,53 @@ function vn(){
     </select>\
     </div> \
     </div>'
-    return kd;
+    return morning;
     }
 
+    function va(){
 
+        var midday='<div class=" col-md-6 "> \
+        <label for="Reminder-choice">Reminders:</label> \
+            <input list="Reminders-list" id="Reminder-choice" name="Reminder-choice" /> \
+                        <!-- <select  > --> \
+                            <datalist id="Reminders-list"> \
+                    <option value="Drink Water">Drink Water</option> \
+                    <option value="Take your Medicines">Take your Medicines</option> \
+                    <option value="Eat">Eat</option> \
+                    <option value="Feed Family">Feed Family</option> \
+                    <option value="Take a 5 min walk">Take a 5 min walk</option> \
+                    <option value="Take a Breather">Take a Breather</option> \
+                    <option value="Take 30 mins for yourself">Take 30 mins for yourself</option> \
+                    <option value="Say something positive about your day or yourself">Say something positive about your day or yourself</option> \
+                    <option value="Walk your pet">Walk your pet</option> \
+                    <option value="You are beautiful, smart & loved">You are beautiful, smart & loved</option> \
+                    <option value="Other">Other</option> \
+                </datalist> \
+                        </div> \
+        <div class = "col-md-3  "> \
+        <select >\
+            <option selected="selected" value="">Select Hour</option>\
+            <option value="12">12</option>\
+            <option value="01">01</option>\
+            <option value="02">02</option>\
+            <option value="03">03</option>\
+            <option value="04">04</option>\
+            <option value="05">05</option>\
+        </select>\
+        <select >\
+            <option selected="selected" value="">Select Minute</option>\
+            <option value="00">00</option>\
+            <option value="30">30</option>\
+        </select>\
+        <select id="selAmPm">\
+            <option selected="selected" value="">AM/PM</option>\
+        <!-- <option value="AM">AM</option> -->\
+        <!-- <option value="PM">PM</option> -->\
+        </select>\
+        </div> \
+        </div>'
+        return midday;
+        }
 
 
 // kd.repeat(2);
